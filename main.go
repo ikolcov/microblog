c7cf56096febf1ad24d1793e78de024d359ab9ce
Ivan Koltsov <ikolcov@yandex.ru>
2023-04-23 17:53:48 +0000

    wip

--- main.go
+++ main.go
@@ -1,18 +1,10 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
-	"fmt"
-	"net/http"
 	"os"
 	"strconv"
-	"time"
 
-	"github.com/gorilla/mux"
-	"github.com/ikolcov/microblog/internal/models"
-	"github.com/ikolcov/microblog/internal/storage"
-	"github.com/ikolcov/microblog/internal/utils"
+	"github.com/ikolcov/microblog/internal/app"
 )
 
 func getServerPort() uint16 {
@@ -25,36 +17,9 @@ func getServerPort() uint16 {
 }
 
 func main() {
-	port := getServerPort()
-	router := mux.NewRouter()
+	config := app.AppConfig{
+		Port: getServerPort(),
+	}
 
-	s := storage.NewPostsStorage()
-	router.HandleFunc("/api/v1/posts", func(w http.ResponseWriter, r *http.Request) {
-		var post models.Post
-		decoder := json.NewDecoder(r.Body)
-		if err := decoder.Decode(&post); err != nil {
-			utils.BadRequest(w, err.Error())
-			return
-		}
-
-		post.AuthorId = models.UserID(r.Header.Get("System-Design-User-Id"))
-		post.CreatedAt = time.Now().Format("2006-01-02T15:04:05Z")
-
-		postId, err := s.AddPost(post)
-		if errors.Is(err, storage.ErrUnauthorized) {
-			utils.Unauthorized(w, err.Error())
-			return
-		} else if err != nil {
-			utils.BadRequest(w, err.Error())
-			return
-		}
-		post.Id = postId
-
-		err = utils.RespondJSON(w, http.StatusOK, post)
-		if err != nil {
-			utils.BadRequest(w, err.Error())
-			return
-		}
-	}).Methods("POST")
-	http.ListenAndServe(fmt.Sprintf(":%v", port), router)
+	app.New(config).Start()
 }
